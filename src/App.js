76496be43828625676749c8e16e5ada866e12f7c
itop55 <bachida.imihi@gmail.com>
2022-10-12 12:28:25 +0300

    инициализация проекта

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import './App.css';
+import './scss/App.css';
+import Header from "./shared/components/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          it_company
-        </a>
-      </header>
+        <Header />
     </div>
   );
 }
---
+++ src/shared/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+        <div className='header'>
+            <div className='header container'>
+                <div className='header__logo'></div>
+                <div className='header__desc'></div>
+                <div className='header__menu'></div>
+                <div className='header__button'></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
