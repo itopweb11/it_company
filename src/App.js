d320aab1ba7e16324923ca1c4a416a6fc4cd4d7d
itop55 <bachida.imihi@gmail.com>
2022-10-14 09:32:34 +0300

    инициализация проекта

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './scss/App.css';
+import './scss/App.scss';
 import Header from "./shared/components/header";
 
 function App() {
--- src/shared/components/header.jsx
+++ src/shared/components/header.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import logo from "../img/logo1.jpg"
+import logoIcon from "../img/logoIcon1.jpg"
+import gear from "../img/gear.png"
+import customer from "../img/customer.png"
+import document from "../img/document.png"
+import telephone from "../img/telephone.png"
+import { Button } from '@material-ui/core';
 
 const Header = () => {
 
     return(
-        <div className='header'>
-            <div className='header container'>
-                <div className='header__logo'></div>
-                <div className='header__desc'></div>
-                <div className='header__menu'></div>
-                <div className='header__button'></div>
+        <div className='header container'>
+            <div className="header__info">
+                <div className='header__logo'>
+                    <img src={logoIcon} alt="logoIcon"/>
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className='header__desc'>
+                    <p>создание и продвижение сайтов</p>
+                </div>
+            </div>
+            <div className="header__info">
+                <div className='header__menu'>
+                    <img src={customer} alt="customer"/>
+                    <a href="#">Разработка</a>
+                    <img src={gear} alt="gear"/>
+                    <a href="#">Поддержка</a>
+                    <img src={document} alt="document"/>
+                    <a href="#">О компании</a>
+                    <img src={telephone} alt="telephone"/>
+                    <a href="#">Контакты</a>
+                </div>
+                <div className="header__button">
+                    <Button variant="contained" color="secondary">
+                        Онлайн-заявка
+                    </Button>
+                </div>
             </div>
         </div>
     )
